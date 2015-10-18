421db5e8234180aa6642b9c959bcaceb77eaa781
Caliog <caliog@caliog.org>
2015-10-18 19:09:11 +0200

    Format + change isMobClass method

--- src/org/caliog/myRPG/Utils/EntityUtils.java
+++ src/org/caliog/myRPG/Utils/EntityUtils.java
@@ -1,27 +1,26 @@
 package org.caliog.myRPG.Utils;
 
+import java.io.File;
+
 import org.bukkit.ChatColor;
-import org.caliog.myRPG.Mobs.MobInstance;
 
 public class EntityUtils {
-    public static String getBar(double h, double mh) {
-	double p = h / mh;
-	mh = 16.0D;
-	String bar = "";
-	for (double i = 1.0D; i <= mh; i += 1.0D) {
-	    if (i / mh <= p) {
-		bar = bar + ChatColor.RED + "♥";
-	    } else {
-		bar = bar + ChatColor.GOLD + "♥";
-	    }
+	public static String getBar(double h, double mh) {
+		double p = h / mh;
+		mh = 16.0D;
+		String bar = "";
+		for (double i = 1.0D; i <= mh; i += 1.0D) {
+			if (i / mh <= p) {
+				bar = bar + ChatColor.RED + "♥";
+			} else {
+				bar = bar + ChatColor.GOLD + "♥";
+			}
+		}
+		return bar;
 	}
-	return bar;
-    }
 
-    public static boolean isMobClass(String name) {
-	if (new MobInstance(name, null, null).mobConfig != null) {
-	    return true;
+	public static boolean isMobClass(String name) {
+		File f = new File(FilePath.mobs + name + ".yml");
+		return f.exists();
 	}
-	return false;
-    }
 }
