fc5a5dc6e5d4d60bde46f8b9a625a16a484c0d05
Caliog <caliog@caliog.org>
2016-02-01 11:40:25 +0100

    Format

--- src/org/caliog/myRPG/Commands/Utils/Command.java
+++ src/org/caliog/myRPG/Commands/Utils/Command.java
@@ -1,131 +1,130 @@
 package org.caliog.myRPG.Commands.Utils;
 
-import org.caliog.myRPG.Utils.myUtils;
-
 import org.bukkit.entity.Player;
+import org.caliog.myRPG.Utils.myUtils;
 
 public class Command {
 
-    private int min, max;
-    private String cmd;
-    private CommandField[] fields;
-    private String permission;
-    private CommandExecutable exe;
+	private int min, max;
+	private String cmd;
+	private CommandField[] fields;
+	private String permission;
+	private CommandExecutable exe;
 
-    private Command(String cmd, String permission, CommandField... fields) {
-	this.fields = fields;
-	this.min = calcMin(fields);
-	this.max = calcMax(fields);
-	this.cmd = cmd;
-	if (permission == null)
-	    this.permission = null;
-	else
-	    this.permission = new String(permission);
-    }
-
-    private int calcMax(CommandField[] fields2) {
-	if (fields2 == null)
-	    return 0;
-	return fields2.length;
-    }
-
-    public Command(String name, String permission, CommandExecutable executor, int max, CommandField... fields) {
-	this(name, permission, executor, fields);
-	this.setMax(max);
-    }
-
-    public Command(String name, String permission, CommandExecutable executor, CommandField... fields) {
-	this(name, permission, fields);
-	this.setExe(executor);
-
-    }
-
-    private int calcMin(CommandField[] fields2) {
-	int r = 0;
-	if (fields2 == null)
-	    return 0;
-	for (CommandField field : fields2) {
-	    if (field.isRequired()) {
-		r++;
-	    }
+	private Command(String cmd, String permission, CommandField... fields) {
+		this.fields = fields;
+		this.min = calcMin(fields);
+		this.max = calcMax(fields);
+		this.cmd = cmd;
+		if (permission == null)
+			this.permission = null;
+		else
+			this.permission = new String(permission);
 	}
-	return r;
-    }
 
-    public CommandField[] getIdentifiers() {
-	CommandField[] ident = null;
-	for (CommandField field : fields)
-	    if (!field.isIdentifier())
-		continue;
-	    else
-		ident = (CommandField[]) myUtils.addElementToArray(ident, field);
+	private int calcMax(CommandField[] fields2) {
+		if (fields2 == null)
+			return 0;
+		return fields2.length;
+	}
 
-	return ident;
-    }
+	public Command(String name, String permission, CommandExecutable executor, int max, CommandField... fields) {
+		this(name, permission, executor, fields);
+		this.setMax(max);
+	}
 
-    public void setMax(int max) {
-	this.max = max;
-    }
+	public Command(String name, String permission, CommandExecutable executor, CommandField... fields) {
+		this(name, permission, fields);
+		this.setExe(executor);
 
-    public int getMax() {
-	return max;
-    }
+	}
 
-    public int getMin() {
-	return min;
-    }
+	private int calcMin(CommandField[] fields2) {
+		int r = 0;
+		if (fields2 == null)
+			return 0;
+		for (CommandField field : fields2) {
+			if (field.isRequired()) {
+				r++;
+			}
+		}
+		return r;
+	}
 
-    public void setMin(int min) {
-	this.min = min;
-    }
+	public CommandField[] getIdentifiers() {
+		CommandField[] ident = null;
+		for (CommandField field : fields)
+			if (!field.isIdentifier())
+				continue;
+			else
+				ident = (CommandField[]) myUtils.addElementToArray(ident, field);
 
-    public CommandField[] getFields() {
-	return fields;
-    }
+		return ident;
+	}
 
-    public void setFields(CommandField[] fields) {
-	this.fields = fields;
-    }
+	public void setMax(int max) {
+		this.max = max;
+	}
 
-    public String getPermission() {
-	return permission;
-    }
+	public int getMax() {
+		return max;
+	}
 
-    public void setPermission(String permission) {
-	this.permission = permission;
-    }
+	public int getMin() {
+		return min;
+	}
 
-    public String getName() {
-	return cmd;
-    }
+	public void setMin(int min) {
+		this.min = min;
+	}
 
-    public void setName(String name) {
-	this.cmd = name;
-    }
+	public CommandField[] getFields() {
+		return fields;
+	}
 
-    public CommandExecutable getExe() {
-	return exe;
-    }
+	public void setFields(CommandField[] fields) {
+		this.fields = fields;
+	}
 
-    public void setExe(CommandExecutable exe) {
-	this.exe = exe;
-    }
+	public String getPermission() {
+		return permission;
+	}
 
-    public String getUsage() {
-	String fields = "";
-	for (CommandField field : this.fields)
-	    if (field.isIdentifier())
-		fields += field.getName() + " ";
-	    else if (field.isRequired())
-		fields += "<" + field.getName() + "> ";
-	    else if (field.isOptional())
-		fields += "[" + field.getName() + "] ";
+	public void setPermission(String permission) {
+		this.permission = permission;
+	}
 
-	return "/" + this.cmd + " " + fields.trim();
-    }
+	public String getName() {
+		return cmd;
+	}
 
-    public void execute(String[] args, Player player) {
-	getExe().execute(args, player);
+	public void setName(String name) {
+		this.cmd = name;
+	}
 
-    }
+	public CommandExecutable getExe() {
+		return exe;
+	}
+
+	public void setExe(CommandExecutable exe) {
+		this.exe = exe;
+	}
+
+	public String getUsage() {
+		String fields = "";
+		for (CommandField field : this.fields)
+			if (field.isIdentifier())
+				fields += field.getName() + " ";
+			else if (field.isRequired())
+				fields += "<" + field.getName() + "> ";
+			else if (field.isOptional())
+				fields += "[" + field.getName() + "] ";
+
+		return "/" + this.cmd + " " + fields.trim();
+	}
+
+	public void execute(String[] args, Player player) {
+		getExe().execute(args, player);
+
+	}
 }
--- src/org/caliog/myRPG/Spells/SpellLoader.java
+++ src/org/caliog/myRPG/Spells/SpellLoader.java
@@ -20,66 +20,65 @@ import org.caliog.myRPG.Utils.FilePath;
 
 public class SpellLoader {
 
-    protected static ClassLoader classLoader;
-    private static Set<String> paths = new HashSet<String>();
+	protected static ClassLoader classLoader;
+	private static Set<String> paths = new HashSet<String>();
 
-    public static void init() throws IOException {
-	File dir = new File(FilePath.spells);
+	public static void init() throws IOException {
+		File dir = new File(FilePath.spells);
 
-	List<URL> urls = new ArrayList<URL>();
-	for (String file : dir.list()) {
-	    if (file.endsWith(".jar")) {
-		File f = new File(dir, file);
+		List<URL> urls = new ArrayList<URL>();
+		for (String file : dir.list()) {
+			if (file.endsWith(".jar")) {
+				File f = new File(dir, file);
 
-		JarFile jar = new JarFile(f);
-		Enumeration<JarEntry> entries = jar.entries();
-		while (entries.hasMoreElements()) {
-		    JarEntry e = entries.nextElement();
-		    if (e.getName().equalsIgnoreCase("spell.info")) {
-			BufferedReader reader = new BufferedReader(new InputStreamReader(jar.getInputStream(e)));
-			String line;
-			while ((line = reader.readLine()) != null) {
-			    paths.add(line.replaceAll(".java", ""));
+				JarFile jar = new JarFile(f);
+				Enumeration<JarEntry> entries = jar.entries();
+				while (entries.hasMoreElements()) {
+					JarEntry e = entries.nextElement();
+					if (e.getName().equalsIgnoreCase("spell.info")) {
+						BufferedReader reader = new BufferedReader(new InputStreamReader(jar.getInputStream(e)));
+						String line;
+						while ((line = reader.readLine()) != null) {
+							paths.add(line.replaceAll(".java", ""));
+						}
+
+					}
+				}
+				urls.add(f.toURI().toURL());
+				jar.close();
+
 			}
-
-		    }
 		}
-		urls.add(f.toURI().toURL());
-		jar.close();
 
-	    }
-	}
-
-	//defaults
-	paths.add("org.caliog.myRPG.Spells.SpeedSpell");
-	paths.add("org.caliog.myRPG.Spells.InvisibleSpell");
-	classLoader = URLClassLoader.newInstance(urls.toArray(new URL[urls.size()]), Manager.plugin.getClass()
-		.getClassLoader());
-
-    }
-
-    public static Spell load(myClass player, String name) {
-	String mainC = null;
-	if (name == null)
-	    return null;
-	if ((name.equals("Speed") || name.equals("Invisible")) && !name.endsWith("Spell"))
-	    name += "Spell";
-	for (String path : paths) {
-	    if (path.endsWith(name))
-		mainC = path;
-	}
-	if (mainC == null)
-	    return null;
-	try {
-	    Class<?> c = classLoader.loadClass(mainC);
-	    Class<? extends Spell> spellC = c.asSubclass(Spell.class);
-	    Spell spell = spellC.getConstructor(player.getClass()).newInstance(player);
-	    return spell;
-	} catch (Exception e) {
-	    Manager.plugin.getLogger().warning("Failed to load Spell: " + name);
-	    e.printStackTrace();
+		// defaults
+		paths.add("org.caliog.myRPG.Spells.SpeedSpell");
+		paths.add("org.caliog.myRPG.Spells.InvisibleSpell");
+		classLoader = URLClassLoader.newInstance(urls.toArray(new URL[urls.size()]), Manager.plugin.getClass().getClassLoader());
 
 	}
-	return null;
-    }
+
+	public static Spell load(myClass player, String name) {
+		String mainC = null;
+		if (name == null)
+			return null;
+		if ((name.equals("Speed") || name.equals("Invisible")) && !name.endsWith("Spell"))
+			name += "Spell";
+		for (String path : paths) {
+			if (path.endsWith(name))
+				mainC = path;
+		}
+		if (mainC == null)
+			return null;
+		try {
+			Class<?> c = classLoader.loadClass(mainC);
+			Class<? extends Spell> spellC = c.asSubclass(Spell.class);
+			Spell spell = spellC.getConstructor(player.getClass()).newInstance(player);
+			return spell;
+		} catch (Exception e) {
+			Manager.plugin.getLogger().warning("Failed to load Spell: " + name);
+			e.printStackTrace();
+
+		}
+		return null;
+	}
 }
